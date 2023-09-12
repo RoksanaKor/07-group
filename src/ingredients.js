50c5e6619ccc2e78f4168ca801bffa9e81705248
WeronikaRzasa <weronika.rzasa@onet.pl>
2023-09-12 15:30:32 +0200

    css changes

---
+++ src/ingredients.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='ingredients']"),
+    closeModalBtn: document.querySelector("[data-modal-close='ingredients']"),
+    modal: document.querySelector("[data-modal='ingredients']"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
