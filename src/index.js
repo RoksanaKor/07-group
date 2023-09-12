b3bbf9adcb31c9285f94bb789f8c698168150803
WeronikaRzasa <weronika.rzasa@onet.pl>
2023-09-12 13:52:04 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// import ('./folder z modalem')
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open='products']"),
+      closeModalBtn: document.querySelector("[data-modal-close='products']"),
+      modal: document.querySelector("[data-modal='products']"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
