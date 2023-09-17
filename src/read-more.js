795ffe301a73699f01159efdf92c44307df0c59f
romanhalaichuk <vi2virbien@gmail.com>
2023-09-17 11:33:39 +0200

    qa

--- src/read-more.js
+++ src/read-more.js
@@ -1,11 +1,9 @@
-
-(() => {
+document.addEventListener('DOMContentLoaded', function () {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open='read-more']"),
     closeModalBtn: document.querySelector("[data-modal-close='read-more']"),
     modal: document.querySelector("[data-modal='read-more']"),
   };
-
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -13,5 +11,4 @@
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
-})();
-
+});
